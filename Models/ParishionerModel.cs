b317581ee2ee3c508bcdfde7bef0dc45f237a949
KuzinMaksim <kuzin-maksim02@yandex.ru>
2022-04-08 14:15:42 +0400

    Added attributes

--- Models/ParishionerModel.cs
+++ Models/ParishionerModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,16 +10,18 @@ namespace LabsTPV2._3.Models
     public class ParishionerModel
     {
         [Required]
+        [DisplayName("ID")]
         public int ID { get; set; }
-
+        [DisplayName("Name")]
         public string Name { get; set; }
-
+        [DisplayName("Surname")]
         public string Surname { get; set; }
-
+        [DisplayName("BirthDate")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
-
-        public string Adress { get; set; }
-
+        [DisplayName("Adress")]
+        public string Address { get; set; }
+        [DisplayName("Sex")]
         public bool Sex { get; set; }
 
     }
