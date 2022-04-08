67b566f7574c0b8a66d7af6793d103860374b53c
KuzinMaksim <kuzin-maksim02@yandex.ru>
2022-04-08 12:14:02 +0400

    Добавьте файлы проекта.

---
+++ Controllers/ParishionerController.cs
@@ -0,0 +1,31 @@
+﻿using LabsTPV2._3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabsTPV2._3.Controllers
+{
+    public class ParishionerController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ParishionerModel parishionerModel)
+        {
+            if (ModelState.IsValid)
+                return View("Result",parishionerModel);
+            else
+                return View();            
+        }
+        public ActionResult Edit(ParishionerModel parishioner)
+        {
+            return View(model:parishioner);
+        }
+        
+    }
+}
---
+++ Models/ParishionerModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabsTPV2._3.Models
+{
+    public class ParishionerModel
+    {
+        [Required]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Adress { get; set; }
+
+        public bool Sex { get; set; }
+
+    }
+}
